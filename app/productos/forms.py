ed271f75ac794f005ea6624536b6c765b635a5e2
BrayanAlfonso <brayanpaloma19b@gmail.com>
2023-08-24 11:52:17 -0500

    Creacion del proyecto estructurado

---
+++ app/productos/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+
+class NewProductForm(FlaskForm):
+    nombre = StringField('Ingrese producto: ')
+    precio = StringField('Ingrese precio: ')
+    submit = SubmitField('Registrar')
